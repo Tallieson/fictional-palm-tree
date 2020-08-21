f711fd38c1b67dae5fc9504609386361d6afb79e
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-08-20 20:47:10 -0700

    Backlog of commits from class

--- js_chatroom/index.js
+++ js_chatroom/index.js
@@ -1,89 +1,98 @@
-const http = require('http')
-const fs = require('fs')
 const path = require('path')
-const st = require('st') // st is a useful module to make serving static files easier
-const Router = require('http-hash-router') // this module makes defining HTTP routes easier
-
+const express = require('express')
+app = express()
 const port = 8000
 
-// This is where messages will be stored.
-const MESSAGES_PATH = './messages.txt'
+// app.use(express.static(path.join(__dirname + 'static')))
 
-const mount = st({ path: path.join(__dirname, '/static'), url: '/static' })
-const router = Router()
+app.listen(port, () => console.log(`Server runnin on ${port}`))
 
-// going to /static/<file-name> will serve the file <file-name>
-router.set('/static/*', mount)
+// const http = require('http')
+// const fs = require('fs')
+// const path = require('path')
+// const st = require('st') // st is a useful module to make serving static files easier
+// const Router = require('http-hash-router') // this module makes defining HTTP routes easier
 
-// requests to /messages are either a GET or a POST
-router.set('/messages', function (req, res) {
-  if (req.method === 'GET') {
-    getMessages(req, res)
-  } else if (req.method === 'POST') {
-    postMessage(req, res)
-  } else {
-    res.statusCode = 400
-    res.end('unsupported operation')
-  }
-})
+// const port = 8000
 
-// this is the POST handler for /messages
-// this function should write a new message to the file
-function postMessage (req, res) {
-  let data = ''
-  req.on('data', function (chunk) {
-    data += chunk
-  })
+// // This is where messages will be stored.
+// const MESSAGES_PATH = './messages.txt'
 
-  req.on('end', function () {
-    fs.appendFile(MESSAGES_PATH, "\n" + data, (err) => {
-        if (err){
-            console.log(err)
-            res.statusCode = 500
-            res.end('Message failed to post')
-        }
-        else{
-            res.statusCode = 200
-            res.end('Message posted successfully')
-        }
+// const mount = st({ path: path.join(__dirname, '/static'), url: '/static' })
+// const router = Router()
+
+// // going to /static/<file-name> will serve the file <file-name>
+// router.set('/static/*', mount)
+
+// // requests to /messages are either a GET or a POST
+// router.set('/messages', function (req, res) {
+//   if (req.method === 'GET') {
+//     getMessages(req, res)
+//   } else if (req.method === 'POST') {
+//     postMessage(req, res)
+//   } else {
+//     res.statusCode = 400
+//     res.end('unsupported operation')
+//   }
+// })
+
+// // this is the POST handler for /messages
+// // this function should write a new message to the file
+// function postMessage (req, res) {
+//   let data = ''
+//   req.on('data', function (chunk) {
+//     data += chunk
+//   })
+
+//   req.on('end', function () {
+//     fs.appendFile(MESSAGES_PATH, "\n" + data, (err) => {
+//         if (err){
+//             console.log(err)
+//             res.statusCode = 500
+//             res.end('Message failed to post')
+//         }
+//         else{
+//             res.statusCode = 200
+//             res.end('Message posted successfully')
+//         }
         
-    })
-    // After writing to the file, we need to send up a response
-  })
-}
+//     })
+//     // After writing to the file, we need to send up a response
+//   })
+// }
 
-// this is the GET handler for /messages
-// this function should respond with the list of messages
-function getMessages (req, res) {
-  // TODO: write code here to get your messages from the file
-  // hint: use fs.readFile
-  fs.readFile(MESSAGES_PATH, 'utf8',(err, data) => {
-      if(err){
-          console.log(err)
-          res.statusCode = 500
-          res.end('Failed to get messages')
-        }
-        else{
-// you will need to split on newlines, filter out empty strings, and JSON.parse each object
-      const messages = data
-        .split('\n')
-        .filter(line => line) // truthy falsy check to make sure the line of the text file is present to filter out empty values
-        .map(JSON.parse)
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(messages))
-    }
-    })
-}
+// // this is the GET handler for /messages
+// // this function should respond with the list of messages
+// function getMessages (req, res) {
+//   // TODO: write code here to get your messages from the file
+//   // hint: use fs.readFile
+//   fs.readFile(MESSAGES_PATH, 'utf8',(err, data) => {
+//       if(err){
+//           console.log(err)
+//           res.statusCode = 500
+//           res.end('Failed to get messages')
+//         }
+//         else{
+// // you will need to split on newlines, filter out empty strings, and JSON.parse each object
+//       const messages = data
+//         .split('\n')
+//         .filter(line => line) // truthy falsy check to make sure the line of the text file is present to filter out empty values
+//         .map(JSON.parse)
+//       res.writeHead(200, { 'Content-Type': 'application/json' })
+//       res.end(JSON.stringify(messages))
+//     }
+//     })
+// }
 
-const server = http.createServer((req, res) => {
-  router(req, res, {}, function onError (err) {
-    if (err) {
-      res.statusCode = err.statusCode || 500
-      res.end(err.message)
-    }
-  })
-})
+// const server = http.createServer((req, res) => {
+//   router(req, res, {}, function onError (err) {
+//     if (err) {
+//       res.statusCode = err.statusCode || 500
+//       res.end(err.message)
+//     }
+//   })
+// })
 
-server.listen(port)
+// server.listen(port)
 
-console.log('server listening on port:', port)
+// console.log('server listening on port:', port)
--- socket_tutorial/index.js
+++ socket_tutorial/index.js
@@ -1,8 +1,27 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
+    // __dirname returns directory of current file. So this serves the html file in the same folder as a response
   res.sendFile(__dirname + '/index.html');
+});
+
+// Socket connection event
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    // Disconnection event
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+  
+//log out chat messages and emit it to every open socket, including senders
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, () => {
