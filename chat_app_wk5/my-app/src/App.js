64e3f2d5964eee25d76a1d0fda24988badd92a65
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-08-31 19:56:49 -0700

    Chat functionality functioning.

--- chat_app_wk5/my-app/src/App.js
+++ chat_app_wk5/my-app/src/App.js
@@ -1,47 +1,123 @@
 /* globals prompt fetch */
-import{ Chat } from './components'
-const yo = require('yo-yo')
+import React from 'react'
+import Chat from './components'
+
 const io = require('socket.io-client')
 const socket = io()
-
 const nickname = prompt('Enter your nickname:')
 
-
-class App extends React.Component {}
-
-
-socket.on('chat message', msg => {
-  console.log('Got a message:', msg)
-  updateState('messages', state.messages.concat(msg))
-})
-
-const sendForm = document.getElementById('send-message')
-const messageTextField = document.getElementById('message-text')
-sendForm.onsubmit = evt => {
-  evt.preventDefault()
-  const message = { text: messageTextField.value, nick: nickname, room: state.room, date: new Date() }
-  socket.emit('chat message', message)
+function Message (props){
+  return( 
+  <li>
+    <span>{props.message.nick} : </span>{props.message.text}
+    </li>
+  )
 }
 
-const state = {
-  room: '',
-  messages: []
+function Chatter(props) {
+  console.log(props.messages)
+  return (
+    <ul>
+      {props.messages.map((msg, index) => <Message key={index} message={msg}/>)}
+    </ul>
+  )
 }
 
-function updateState (key, value) {
-  state[key] = value
-  yo.update(el, Chat(state.messages, state.room, updateState))
+class MessageForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      text: '',
+      nick: props.nick,
+      room: props.room,
+      date: ''
+    }
+    // 
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    // gets us acess to the sendMessage function, now with 'Apps' parameters bound to it
+    this.sendMessage = this.props.func
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.sendMessage({
+      "text":this.state.text,
+      "nick":this.state.nick,
+      "room":this.state.room,
+      "date":new Date(),
+    })
+    this.setState({
+      text: ''
+    })
+  }
+
+  render(){
+    return(
+      <form onSubmit= {this.handleSubmit}>
+        <input type='text' value={this.state.text} onChange={this.handleChange}></input>
+        <input type='submit' value='Send Message'></input>
+      </form>
+    )
+  }
 }
 
-const el = Chat(state.messages, state.room, updateState)
-const chatContainer = document.getElementById('chat-container')
-chatContainer.appendChild(el)
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+    messages: [],
+    nick: nickname,
+    room: "home"
+  }
+  //binding this function uses the state of this form, rather then the state of where it's called
+  // otherwise it would only have access to the state in the constructor it is called from
+  this.sendMessage = this.sendMessage.bind(this)
 
-// Get initial list of messages
-fetch('/messages')
-  .then(response => response.json())
-  .then(data => {
-    console.log('fetched data from server')
-    updateState('messages', data)
+  socket.on('chat message', msg => {
+    console.log('Got a message:', msg)
+    let finalMessages = this.state.messages
+    finalMessages.push(msg)
+    this.setState({
+      messages: finalMessages
+    })
+
   })
+  }
 
+  sendMessage(finalMessage){
+    console.log(finalMessage)
+    socket.emit('chat message', finalMessage)
+  }
+
+ //runs as soon as the component is mounted to the DOM
+  componentDidMount(){
+    fetch('/messages')
+    .then(response => response.json())
+    .then(messages => {
+      console.log(messages)
+      this.setState({messages: messages})
+    })
+  }
+
+  render(){  
+    console.log(this.state)
+    return(
+    <div>
+        <Chatter messages={this.state.messages}/>
+        {/* passing sendMessage so we can get access to this constructors state where it is called */}
+        <MessageForm nick={this.state.nick} room={this.state.room} func={this.sendMessage}/>
+    </div>
+    )
+  }
+}
+
+
+export default App
+  
