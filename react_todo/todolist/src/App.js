13a95f416015ab6015f7066c7d1979e586e6efd0
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-09-02 18:01:43 -0700

    attempting to implement reactdom

--- react_todo/todolist/src/App.js
+++ react_todo/todolist/src/App.js
@@ -1,26 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All</Link>
+            </li>
+            <li>
+              <Link to="/todo">Todo</Link>
+            </li>
+            <li>
+              <Link to="/done">Done</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <All />
+          </Route>
+          <Route path="/todo">
+            <Todo />
+          </Route>
+          <Route path="/done">
+            <Done />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function All() {
+  return <h2>All</h2>;
+}
+
+function Todo() {
+  return <h2>Todo</h2>;
+}
+
+function Done() {
+  return <h2>Done</h2>;
+}
+
+
+
+
+// class todoForm extends React.Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       text: ''
+//     }
+//     // 
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//     // gets us acess to the sendTodo function, now with 'Apps' parameters bound to it
+//     this.sendTodo = this.props.func
+//   }
+
+//   handleChange(event) {
+//     this.setState({
+//       text: event.target.value
+//     })
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault()
+//     this.sendTodo({
+//       "text":this.state.text
+//     })
+//     this.setState({
+//       text: ''
+//     })
+//   }
+
+//   render(){
+//     return(
+//       <form onSubmit= {this.handleSubmit}>
+//         <input type='text' value={this.state.text} onChange={this.handleChange}></input>
+//         <input type='submit' value='Add Todo'></input>
+//       </form>
+//     )
+//   }
+// }
+
+// class App extends React.Component {
+//   constructor(props){
+//     super(props)
+//     this.state ={
+//     todos: []
+//   }
+//   //binding this function uses the state of this form, rather then the state of where it's called
+//   // otherwise it would only have access to the state in the constructor it is called from
+//   this.sendTodo = this.sendTodo.bind(this)
+// }
+
+//   sendTodo(finalMessage){
+//     socket.emit('chat message', finalMessage)
+//   }
+
+//  //runs as soon as the component is mounted to the DOM
+//   componentDidMount(){
+//     fetch('/todos')
+//     .then(response => response.json())
+//     .then(todos => {
+//       this.setState({todos: todos})
+//     })
+//     socket.on('chat message', msg => {
+//       console.log('Got a message:', msg)
+//       let finalMessages = this.state.messages
+//       finalMessages.push(msg)
+//       this.setState({
+//         messages: finalMessages
+//       })
+//     })
+//   }
+
+//   render(){  
+//     return(
+//     <div>
+//         <Chatter messages={this.state.messages}/>
+//         {/* passing sendTodo so we can get access to this constructors state where it is called */}
+//         <MessageForm nick={this.state.nick} room={this.state.room} func={this.sendTodo}/>
+//     </div>
+//     )
+//   }
+// }
+
