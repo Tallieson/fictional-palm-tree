e703806d621862d5d6db6a113aff71c476d38856
Talieson <54685909+Tallieson@users.noreply.github.com>
2020-09-03 18:43:56 -0700

    Starting week 6 chat app

--- react_todo/todolist/src/App.js
+++ react_todo/todolist/src/App.js
@@ -6,55 +6,140 @@ import {
   Link
 } from "react-router-dom";
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">All</Link>
-            </li>
-            <li>
-              <Link to="/todo">Todo</Link>
-            </li>
-            <li>
-              <Link to="/done">Done</Link>
-            </li>
-          </ul>
-        </nav>
+import todos from "./todos"
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/">
-            <All />
-          </Route>
-          <Route path="/todo">
-            <Todo />
-          </Route>
-          <Route path="/done">
-            <Done />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+const todolist = todos.todos
+todolist.sort((a, b) => parseFloat(a.id) - parseFloat(b.id))
+let todoNumber = todolist[0].id
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+
+    // allows functions to acess prop values of the component
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    // gets us acess to the sendTodo function, now with 'Apps' parameters bound to it
+    this.sendTodo = this.props.func
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.sendTodo(this.state.text)
+    this.setState({
+      text: ''
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' value={this.state.text} onChange={this.handleChange}></input>
+        <input type='submit' value='Add Todo'></input>
+      </form>
+    )
+  }
 }
 
-function All() {
-  return <h2>All</h2>;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: todolist,
+      numberOfTodos: todoNumber
+    }
+  
+  this.sendTodo = this.sendTodo.bind(this)
+  this.completeTodo = this.completeTodo.bind(this)
+  }
+  
+  completeTodo(id){
+    this.setState({
+    todos: this.state.todos
+    .filter(todo => todo.id === id)
+    .map((specificTodo) => {
+      specificTodo.completed = !specificTodo.completed
+      return specificTodo
+    })
+  })
+  }
+
+  sendTodo(newTodo){
+    this.setState({
+      todos: this.state.todos.concat({
+        "id": this.state.NumberOfTodos + 1,
+        "text": newTodo,
+        "completed": false
+      }),
+      numberOfTodos: this.state.numberOfTodos + 1
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">All</Link>
+              </li>
+              <li>
+                <Link to="/not_done">Not Done</Link>
+              </li>
+              <li>
+                <Link to="/done">Done</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/not_done">
+              <Todos sendTodo={this.sendTodo} completeTodo={this.completeTodo} todos={this.state.todos.filter(todo => !todo.completed)} />
+            </Route>
+            <Route path="/done">
+              <Todos sendTodo={this.sendTodo} completeTodo={this.completeTodo} todos={this.state.todos.filter(todo => todo.completed)} />
+            </Route>
+            <Route path="/">
+              <Todos sendTodo={this.sendTodo} completeTodo={this.completeTodo} todos={this.state.todos} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+
+    function Todos(props) {
+      let listOfTodos = props.todos
+      return (
+        <div>
+          <div>
+            <h2>Todos</h2>
+            {listOfTodos.map((todo) => 
+              <li key={todo.id}>
+                <span>{todo.text}</span>
+                <button onClick={() => props.completeTodo(todo.id)}>✔</button>
+              </li>)}
+          </div>
+          <div>
+            <TodoForm func={props.sendTodo}></TodoForm>
+          </div>
+        </div>
+      )
+    }
+  }
 }
 
-function Todo() {
-  return <h2>Todo</h2>;
-}
-
-function Done() {
-  return <h2>Done</h2>;
-}
-
-
+export default App
+// this.setState({ todos: this.state.todos.concat(newTodo) })
 
 
 // class todoForm extends React.Component {
